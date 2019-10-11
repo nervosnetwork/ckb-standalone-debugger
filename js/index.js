e32832eafee90731b11eee924c4648649123b2c6
Xuejie Xiao <xxuejie@gmail.com>
2019-10-11 05:25:49 +0000

    feat: Switch to npm based debugger loading

--- js/index.js
+++ js/index.js
@@ -1,17 +1,13 @@
 import sample from "../tests/programs/sample.json";
-const rust = import('./pkg/ckb_standalone_debugger');
+import { run_json_with_printer } from "ckb-standalone-debugger";
 
-rust
-  .then(({ run_json_with_printer }) => {
-    const result = run_json_with_printer(
-      JSON.stringify(sample),
-      "type",
-      "0x12bec80f9654173c0362fade816040de30b2a15f53c71e3f60570ca39ef8ebb0",
-      "4000",
-      (hash, message) => {
-        console.log(`Script ${hash} generates debug message: ${message}`);
-      });
-    const result_object = JSON.parse(result);
-    console.log("Result: ", result_object);
-  })
-  .catch(console.error);
+const result = run_json_with_printer(
+  JSON.stringify(sample),
+  "type",
+  "0x12bec80f9654173c0362fade816040de30b2a15f53c71e3f60570ca39ef8ebb0",
+  "4000",
+  (hash, message) => {
+    console.log(`Script ${hash} generates debug message: ${message}`);
+  });
+const result_object = JSON.parse(result);
+console.log("Result: ", result_object);
