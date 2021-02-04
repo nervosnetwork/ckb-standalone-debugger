64fc513bcb04027abf86feeae850b5714d6f6fbb
Xuejie Xiao <xxuejie@gmail.com>
2021-02-04 13:58:42 +0800

    Remove unused JS files for now

--- js/bootstrap.js
+++
@@ -1,3 +0,0 @@
-/* Inspired from https://github.com/rustwasm/wasm_game_of_life/blob/fc35b7cf803ca4c706bb6498ada5754d95daf8e1/www/bootstrap.js */
-import("./index.js")
-  .catch(e => console.error("Error importing `index.js`:", e));
