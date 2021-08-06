f62fc868885d47596e808bfac72a681aeabaa35e
xjd <xjd@cryptape.com>
2021-08-06 11:05:32 +0800

    Add --pprof (performance profiling) (#15)
    * add --pprof (performance profiling)
    * Use optimized version of pprof

---
+++ bins/tests/fib.c
@@ -0,0 +1,25 @@
+// uncomment to enable printf in CKB-VM
+#define CKB_C_STDLIB_PRINTF
+//#define CKB_C_STDLIB_PRINTF_BUFFER_SIZE 1024
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "ckb_syscalls.h"
+
+
+int fib(int n) {
+    if (n == 0 || n == 1) {
+        printf("n = %d", n);
+        return n;
+    } else {
+        printf("n = %d", n);
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+int main() {
+    if (fib(5) != 5) {
+        return 1;
+    }
+    return 0;
+}
