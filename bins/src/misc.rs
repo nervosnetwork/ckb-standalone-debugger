68fe5d802a91ddf6ec741a43b731f6a1d8418f02
mohanson <mohanson@outlook.com>
2021-11-29 15:21:20 +0800

    Change gt => gte

--- bins/src/misc.rs
+++ bins/src/misc.rs
@@ -5,9 +5,9 @@ pub struct HumanReadableCycles(pub u64);
 impl fmt::Display for HumanReadableCycles {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0)?;
-        if self.0 > 1024 * 1024 {
+        if self.0 >= 1024 * 1024 {
             write!(f, "({:.1}M)", self.0 as f64 / 1024. / 1024.)?;
-        } else if self.0 > 1024 {
+        } else if self.0 >= 1024 {
             write!(f, "({:.1}K)", self.0 as f64 / 1024.)?;
         } else {
 
