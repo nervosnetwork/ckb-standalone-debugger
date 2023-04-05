936a5fba3ab09ee4ab30dc23c65c376ce4d35d48
Xuejie Xiao <xxuejie@gmail.com>
2023-04-05 01:24:08 +0000

    De-duplicate proto file

--- ckb-vm-signal-profiler/build.rs
+++ ckb-vm-signal-profiler/build.rs
@@ -1,11 +1,1 @@
-extern crate protoc_rust;
-
-fn main() {
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
-
-    let mut codegen = protoc_rust::Codegen::new();
-    if target_os == "linux" {
-        codegen.protoc_path(protoc_bin_vendored::protoc_bin_path().unwrap());
-    }
-    codegen.out_dir("src/protos").inputs(&["proto/profile.proto"]).include("proto").run().expect("protoc");
-}
+../protos/build.rs
