a45b7995bd160131d41dd1e5652de6646f5ed677
Mohanson <mohanson@outlook.com>
2023-06-27 09:37:32 +0800

    Update spawn tests (#91)

--- ckb-debugger/examples/spawn_caller_strcat.c
+++ ckb-debugger/examples/spawn_caller_strcat.c
@@ -4,23 +4,28 @@
 #include "ckb_syscalls.h"
 
 int main() {
-  const char *argv[] = {"hello", "world"};
-  int8_t exit_code = 255;
-  uint8_t content[80] = {};
-  uint64_t content_length = 80;
-
+  const char*  argv[] = {"hello", "world"};
+  int8_t       spgs_exit_code = 255;
+  uint8_t      spgs_content[80] = {};
+  uint64_t     spgs_content_length = 80;
+  spawn_args_t spgs = {
+    .memory_limit = 8,
+    .exit_code = &spgs_exit_code,
+    .content = &spgs_content[0],
+    .content_length = &spgs_content_length,
+  };
   int success =
-      ckb_spawn(8, 1, 3, 0, 2, argv, &exit_code, &content[0], &content_length);
+      ckb_spawn(1, 3, 0, 2, argv, &spgs);
   if (success != 0) {
     return 1;
   }
-  if (exit_code != 0) {
+  if (spgs_exit_code != 0) {
     return 1;
   }
-  if (strlen((char *)content) != 10) {
+  if (strlen((char *)spgs_content) != 10) {
     return 1;
   }
-  if (strcmp((char *)content, "helloworld") != 0) {
+  if (strcmp((char *)spgs_content, "helloworld") != 0) {
     return 1;
   }
   return 0;
