40773f98c968f66fb33c5ae44111eac1c5d59783
joii2020 <joii@cryptape.com>
2023-07-19 12:38:14 +0800

    optimized decoding

--- ckb-debugger/src/decode.rs
+++ ckb-debugger/src/decode.rs
@@ -1,76 +1,33 @@
-use ckb_vm::ckb_vm_definitions::instructions::{Instruction, InstructionOpcode};
-
-enum ISA {
-    Imc,
-    A,
-    B,
-    Unknow,
-}
-
 pub fn decode_instruction(data: &str) -> Result<(), Box<dyn std::error::Error>> {
     let instruction = if data.starts_with("0x") {
-        u64::from_str_radix(&data[2..], 16)?
+        u32::from_str_radix(&data[2..], 16)?
     } else {
-        u64::from_str_radix(data, 10)?
+        u32::from_str_radix(data, 10)?
     };
 
-    let info = InstructionInfo::new(instruction);
+    use ckb_vm::instructions::*;
+    use ckb_vm::machine::VERSION2;
 
-    println!("instruction: {}", info.get_ins_info());
-    println!("It is in RISC-V {}.", info.get_isa_str());
+    let (ins, isa) = if let Some(i) = i::factory::<u64>(instruction, VERSION2) {
+        let tagged_instruction: tagged::TaggedInstruction = tagged::TaggedInstruction::try_from(i).unwrap();
+        (tagged_instruction.to_string(), "Integer Instruction")
+    } else if let Some(i) = m::factory::<u64>(instruction, VERSION2) {
+        let tagged_instruction = tagged::TaggedInstruction::try_from(i).unwrap();
+        (tagged_instruction.to_string(), "M extension")
+    } else if let Some(i) = a::factory::<u64>(instruction, VERSION2) {
+        let tagged_instruction = tagged::TaggedInstruction::try_from(i).unwrap();
+        (tagged_instruction.to_string(), "A extension")
+    } else if let Some(i) = b::factory::<u64>(instruction, VERSION2) {
+        let tagged_instruction = tagged::TaggedInstruction::try_from(i).unwrap();
+        (tagged_instruction.to_string(), "B extension")
+    } else if let Some(i) = rvc::factory::<u64>(instruction, VERSION2) {
+        let tagged_instruction = tagged::TaggedInstruction::try_from(i).unwrap();
+        (tagged_instruction.to_string(), "RVC")
+    } else {
+        panic!("unknow instruction")
+    };
+
+    println!("instruction: {}\nIt is in RISC-V {}.", ins, isa);
+
     Ok(())
-}
-
-struct InstructionInfo {
-    instruction: Instruction,
-    opcode: InstructionOpcode,
-    isa: ISA,
-}
-
-impl InstructionInfo {
-    pub fn new(instruction: Instruction) -> Self {
-        let opcode = ckb_vm::instructions::extract_opcode(instruction);
-        let isa = Self::get_isa(opcode);
-
-        Self {
-            instruction,
-            opcode,
-            isa,
-        }
-    }
-
-    pub fn get_isa_str(&self) -> &str {
-        match self.isa {
-            ISA::Imc => "Base Instruction",
-            ISA::A => "A extension",
-            ISA::B => "B extension",
-            ISA::Unknow => panic!("Unknow instruction: {}", self.opcode),
-        }
-    }
-
-    pub fn get_ins_info(&self) -> String {
-        use ckb_vm::instructions::*;
-
-        let tagged_instruction_option = tagged::TaggedInstruction::try_from(self.instruction);
-        if let Ok(tagged_instruction) = tagged_instruction_option {
-            tagged_instruction.to_string()
-        } else {
-            panic!("unknown instruction")
-        }
-    }
-
-    fn get_isa(ins_opcode: InstructionOpcode) -> ISA {
-        use ckb_vm::ckb_vm_definitions::instructions::*;
-        if (ins_opcode > OP_UNLOADED && ins_opcode <= OP_XORI)
-            || (ins_opcode >= OP_WIDE_MUL && ins_opcode <= OP_CUSTOM_TRACE_END)
-        {
-            ISA::Imc
-        } else if ins_opcode >= OP_LR_W && ins_opcode <= OP_AMOMAXU_D {
-            ISA::A
-        } else if ins_opcode >= OP_ADDUW && ins_opcode <= OP_ZEXTH {
-            ISA::B
-        } else {
-            ISA::Unknow
-        }
-    }
 }
