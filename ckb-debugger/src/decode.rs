2940a3201506f7391efd3f04904f12e3b380b001
mohanson <mohanson@outlook.com>
2024-06-11 10:25:51 +0800

    Move 'use statement' to the beginning of the file

--- ckb-debugger/src/decode.rs
+++ ckb-debugger/src/decode.rs
@@ -1,10 +1,9 @@
+use ckb_vm::instructions::*;
+use ckb_vm::machine::VERSION2;
+
 pub fn decode_instruction(data: &str) -> Result<(), Box<dyn std::error::Error>> {
     let instruction =
         if data.starts_with("0x") { u32::from_str_radix(&data[2..], 16)? } else { u32::from_str_radix(data, 10)? };
-
-    use ckb_vm::instructions::*;
-    use ckb_vm::machine::VERSION2;
-
     let (ins, isa) = if let Some(i) = i::factory::<u64>(instruction, VERSION2) {
         let tagged_instruction: tagged::TaggedInstruction = tagged::TaggedInstruction::try_from(i).unwrap();
         (tagged_instruction.to_string(), "I")
@@ -14,12 +13,12 @@ pub fn decode_instruction(data: &str) -> Result<(), Box<dyn std::error::Error>> 
     } else if let Some(i) = a::factory::<u64>(instruction, VERSION2) {
         let tagged_instruction = tagged::TaggedInstruction::try_from(i).unwrap();
         (tagged_instruction.to_string(), "A")
-    } else if let Some(i) = b::factory::<u64>(instruction, VERSION2) {
-        let tagged_instruction = tagged::TaggedInstruction::try_from(i).unwrap();
-        (tagged_instruction.to_string(), "B")
     } else if let Some(i) = rvc::factory::<u64>(instruction, VERSION2) {
         let tagged_instruction = tagged::TaggedInstruction::try_from(i).unwrap();
         (tagged_instruction.to_string(), "C")
+    } else if let Some(i) = b::factory::<u64>(instruction, VERSION2) {
+        let tagged_instruction = tagged::TaggedInstruction::try_from(i).unwrap();
+        (tagged_instruction.to_string(), "B")
     } else {
         ("?".to_string(), "?")
     };
