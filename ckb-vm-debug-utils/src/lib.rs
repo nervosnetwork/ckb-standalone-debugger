c778e58544b41d8754a587c2523cb2abe257a11d
mohanson <mohanson@outlook.com>
2024-06-27 16:20:02 +0800

    Remove ckb-gdb-remote-protocol

--- ckb-vm-debug-utils/src/lib.rs
+++ ckb-vm-debug-utils/src/lib.rs
@@ -1,14 +1,11 @@
-#[macro_use]
 extern crate log;
 
 mod elf_dumper;
-mod gdbserver;
 mod gdbstub;
 #[cfg(feature = "stdio")]
 mod stdio;
 
 pub use crate::gdbstub::{GdbStubHandler, GdbStubHandlerEventLoop};
 pub use elf_dumper::ElfDumper;
-pub use gdbserver::GdbHandler;
 #[cfg(feature = "stdio")]
 pub use stdio::Stdio;
