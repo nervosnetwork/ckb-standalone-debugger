fdda0c563db9df9324535902ad989d2326818d7e
mohanson <mohanson@outlook.com>
2024-06-28 15:08:02 +0800

    Move all examples in ./res to ./examples

--- ckb-debugger/examples/MakefileTest.sh
+++ ckb-debugger/examples/MakefileTest.sh
@@ -15,6 +15,8 @@ ckb-debugger --mode full --bin fib --enable-steplog
 ckb-debugger --mode fast --bin fib --max-cycles 100 2>&1 | grep "ExceededMaximumCycles"
 ckb-debugger --mode full --bin fib --max-cycles 100 2>&1 | grep "CyclesExceeded"
 
+ckb-debugger --mode full --bin file_operations | grep "Run result: 0"
+
 ckb-debugger --mode fast --tx-file mock_tx.json --cell-index 0 --cell-type input --script-group-type lock
 ckb-debugger --mode full --tx-file mock_tx.json --cell-index 0 --cell-type input --script-group-type lock
 ckb-debugger --mode full --tx-file mock_tx.json --cell-index 0 --cell-type input --script-group-type lock --bin always_failure 2>&1 | grep "Run result: 1"
@@ -24,6 +26,8 @@ ckb-debugger --mode full --bin out_of_memory 2>&1 | grep "MemOutOfBound"
 
 ckb-debugger --mode fast --bin print_log
 ckb-debugger --mode full --bin print_log
+
+ckb-debugger --bin read_file --read-file read_file.c | grep "Run result: 0"
 
 ckb-debugger --mode fast --tx-file spawn.json --cell-index 0 --cell-type input --script-group-type lock
 ckb-debugger --mode full --tx-file spawn.json --cell-index 0 --cell-type input --script-group-type lock
